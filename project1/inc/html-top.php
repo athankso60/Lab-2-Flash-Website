168f6200ebf93f4980a456f39140cb42587dce85
cmarti21 <55210718+cmarti21@users.noreply.github.com>
2019-09-11 16:26:39 -0400

    Add files via upload

---
+++ project1/inc/html-top.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en"> 
+
+<head> 
+	<meta charset="utf-8">
+	<title>Team Barcelona | Lab 01: Flash Website</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body class="container">
+	<header>  
+		<a href="index.php">
+		<h1> Team Barcelona</h1>
+		<img src="images/team.jpg" alt="Random photo from the internet so this wouldn't be empty">
+		</a>
+	</header>
+
+
+	<aside>
+		<ul>
+			<li>Best team</li>
+			<li>Best fashion</li>
+			<li>Team Barcelona forever</li>	
+		</ul>	
+	</aside>
+</body>
---
+++ project1/inc/navn.php
@@ -0,0 +1,8 @@
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="early_days.php">Early Days</a></li>
+			<li><a href="college.php">College</a></li>
+			<li><a href="future.php">Future</a></li>
+		</ul>	
+	</nav>
