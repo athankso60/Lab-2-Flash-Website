60f445e09a30e382489325b6a79d3bb876106cba
athankso60 <44515526+athankso60@users.noreply.github.com>
2019-09-11 15:56:53 -0400

    Add files via upload

---
+++ andrew/header.php
@@ -0,0 +1,14 @@
+
+	<header>
+
+		<h1>Andrew Thankson</h1>
+		<img src = "images/me.jpeg" alt ="A photo of me"></img>
+
+
+		<aside>
+			Computer Science Student, University of Rochester <br>
+			Freelance Web Developer<br>
+			Entrepreneur
+		</aside>
+		
+	</header>
