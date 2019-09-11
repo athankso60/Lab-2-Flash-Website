5abccd7229e30840be2d8a93a71b8d970e0d6be4
athankso60 <44515526+athankso60@users.noreply.github.com>
2019-09-11 17:35:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <?php include "inc/nav.php"; ?>
 
-<a href = "folder 2/lab01/index.php" >Clara</a>
+	<a href = "folder 2/lab01/index.php" >Clara</a>
 	<a href = "file 1/index.php">Ana</a>
 	<a href = "andrew/index.php">Andrew</a>
 
