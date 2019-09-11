781cd9464422a86e2b2aecf172f240b839c78174
athankso60 <44515526+athankso60@users.noreply.github.com>
2019-09-11 16:21:04 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Our new Website</title>
+</head>
+<body>
+
+	<a href = "folder 2/lab01/index.php" >Clara</a>
+	<a href = "file 1/index.php">Ana</a>
+	<a href = "andrew/index.php">Andrew</a>
+
+</body>
+</html>
