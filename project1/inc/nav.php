dbf90bd5ef98e1283900d365156ab5c3fc48196d
athankso60 <44515526+athankso60@users.noreply.github.com>
2019-09-11 17:49:21 -0400

    Update nav.php

--- project1/inc/nav.php
+++ project1/inc/nav.php
@@ -1,8 +1,8 @@
 	<nav class="home_nav">
 		<ul>
 			<li><a href="index.php">Home</a></li>
-			<li><a href="clara.php">Clara</a></li>
-			<li><a href="andrew.php">Andrew</a></li>
-			<li><a href="ana.php">Ana</a></li>
+			<li><a href="folder 2/lab01/index.php">Clara</a></li>
+			<li><a href="andrew/index.php">Andrew</a></li>
+			<li><a href="file 1/index.php">Ana</a></li>
 		</ul>	
 	</nav>
