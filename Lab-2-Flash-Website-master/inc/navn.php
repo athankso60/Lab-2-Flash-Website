5c597cfcb227e23a7351c7338f2e2409621a0d7a
cmarti21 <55210718+cmarti21@users.noreply.github.com>
2019-09-12 16:41:03 -0400

    Update navn.php

--- Lab-2-Flash-Website-master/inc/navn.php
+++ Lab-2-Flash-Website-master/inc/navn.php
@@ -1,13 +1,9 @@
 
 	<nav>
-
-			<ul>
-				<li><a <?php if($current == 'home') {echo 'class="is-current"';} ?> href="andrew.php">Home</a></li>
-				<li><a <?php if($current == 'earlydays') {echo 'class="is-current"';} ?> href="early_days.php">Early Days</a></li>
-				<li><a <?php if($current == 'college') {echo 'class="is-current"';} ?> href="college.php">College Life</a><li>
-				<li><a <?php if($current == 'future') {echo 'class="is-current"';} ?> href="future.php">Future Plans</a></li>
-			</ul>
-
+		<ul>
+			<li><a <?php if($current == 'home') {echo 'class="is-current"';} ?> href="andrew.php">Home</a></li>
+			<li><a <?php if($current == 'earlydays') {echo 'class="is-current"';} ?> href="early_days.php">Early Days</a></li>
+			<li><a <?php if($current == 'college') {echo 'class="is-current"';} ?> href="college.php">College Life</a><li>
+			<li><a <?php if($current == 'future') {echo 'class="is-current"';} ?> href="future.php">Future Plans</a></li>
+		</ul>
 	</nav>
-
-		<footer>CSC 174: Advanced Front-end Web Design and Development</footer>
