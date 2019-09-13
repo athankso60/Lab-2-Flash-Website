f67547c5d10d246feb167bb653610ac383e108d8
Clara Martinez <cmarti21@u.rochester.edu>
2019-09-13 14:28:55 -0400

    cleaning up again

--- project1/inc/nav.php
+++
@@ -1,8 +0,0 @@
-	<nav class="home_nav">
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="folder 2/lab01/index.php">Clara</a></li>
-			<li><a href="andrew/index.php">Andrew</a></li>
-			<li><a href="file 1/index.php">Ana</a></li>
-		</ul>	
-	</nav>
--- project1/inc/navn.php
+++
@@ -1,8 +0,0 @@
-	<nav>
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="early_days.php">Early Days</a></li>
-			<li><a href="college.php">College</a></li>
-			<li><a href="future.php">Future</a></li>
-		</ul>	
-	</nav>
